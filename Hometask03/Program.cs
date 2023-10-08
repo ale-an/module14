b5d5feedf73f87fe93ba456b483d6d8c720708eb
Alena <vorozhtsova_alena@mail.ru>
2023-10-08 13:20:42 +0500

    Домашняя работа 14.3.3

---
+++ Hometask03/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Hometask03;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var phoneBook = new List<Contact>();
+
+        phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+        phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+        phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+        phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+        phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+        phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+        phoneBook = phoneBook.OrderBy(x => x.Name).ThenBy(x => x.LastName).ToList(); // сортировка по имени, а потом по фамилии
+
+        while (true)
+        {
+            var keyChar = Console.ReadLine();
+
+            var page = Convert.ToInt32(keyChar);
+
+            var pagedContacts = phoneBook.Skip(page * 2 - 2).Take(2);
+
+            foreach (var record in pagedContacts)
+                Console.WriteLine(record.Name + " " + record.LastName + ": " + record.PhoneNumber);
+        }
+
+    }
+}
